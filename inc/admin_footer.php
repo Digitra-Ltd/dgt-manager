f83fe61d2af987ae4b1a6b5c696ba52ade5b9f62
Guy ZINGUI <guyzwyk@gmail.com>
2018-04-30 05:00:05 +0100

    Add files via upload

---
+++ inc/admin_footer.php
@@ -0,0 +1,19 @@
+<div class="clearfix"> </div>
+	      	</div>
+			<div class="copy">
+		    	<p>Copyright &copy; 2015 DIGITRA. All Rights Reserved | Design by <a href="http://www.digitra.com/" target="_blank">Digitra</a> </p>
+			</div>
+		</div>
+      <!-- /#page-wrapper -->
+   </div>
+    <!-- /#wrapper -->
+    
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    
+    <!-- Metis Menu Plugin JavaScript -->
+	<script src="js/metisMenu.min.js"></script>
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
